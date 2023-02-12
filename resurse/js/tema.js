593784c000183d7f30bc3c7e660336cb9be0d8c6
emmac <emma.cobzariu@s.unibuc.ro>
2023-02-12 15:17:35 +0200

    dark theme

--- resurse/js/tema.js
+++ resurse/js/tema.js
@@ -1,10 +1,16 @@
 window.onload=function(){
+temaCurenta=localStorage.getItem("tema")
+
+if(temaCurenta)
+    document.body.classList.add(temaCurenta);
     document.getElementById("tema").onclick=function(){
         if(document.body.classList.contains("dark")){
             document.body.classList.remove("dark");
+            localStorage.removeItem("tema");
         }
         else{
             document.body.classList.add("dark");
+            localStorage.setItem("tema", "dark");
         }
     }
 
