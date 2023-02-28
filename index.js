dbbcbb6c8fffd293faf3557f4fb1d1e7ed9f5e47
emmac <emma.cobzariu@s.unibuc.ro>
2023-02-28 13:51:22 +0200

    eliminare chestii inutile din head

--- index.js
+++ index.js
@@ -91,33 +91,7 @@ app.get(["/","/index","/home"], function(req, res){
                                                                                              //obGlobal.imagini = vector
 });
 
-app.get("*/galerie-animata.css",function(req, res){
 
-    var sirScss=fs.readFileSync(__dirname+"/resurse/scss/galerie_animata.scss").toString("utf8");
-    var culori=["navy","black","purple","grey"];
-    var indiceAleator=Math.floor(Math.random()*culori.length);
-    var culoareAleatoare=culori[indiceAleator]; 
-    rezScss=ejs.render(sirScss,{culoare:culoareAleatoare});
-    console.log(rezScss);
-    var caleScss=__dirname+"/temp/galerie_animata.scss"
-    fs.writeFileSync(caleScss,rezScss);
-    try {
-        rezCompilare=sass.compile(caleScss,{sourceMap:true});
-        
-        var caleCss=__dirname+"/temp/galerie_animata.css";
-        fs.writeFileSync(caleCss,rezCompilare.css);
-        res.setHeader("Content-Type","text/css");
-        res.sendFile(caleCss);
-    }
-    catch (err){
-        console.log(err);
-        res.send("Eroare");
-    }
-});
-
-app.get("*/galerie-animata.css.map",function(req, res){
-    res.sendFile(path.join(__dirname,"temp/galerie-animata.css.map"));
-});
 
 
 app.get("/*.ejs", function(req, res){
