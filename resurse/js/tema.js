6e6245408076c86a6f12c9c9f05858e2d5309690
emmac <emma.cobzariu@s.unibuc.ro>
2023-04-25 19:40:56 +0300

    partial toggle light/dark

--- resurse/js/tema.js
+++ resurse/js/tema.js
@@ -1,17 +1,19 @@
 window.addEventListener("DOMContentLoaded", function(){
-temaCurenta=localStorage.getItem("tema")
-
-if(temaCurenta)
-    document.body.classList.add(temaCurenta);
-    document.getElementById("tema").onclick=function(){
-        if(document.body.classList.contains("dark")){
-            document.body.classList.remove("dark");
-            localStorage.removeItem("tema");
-        }
-        else{
-            document.body.classList.add("dark");
-            localStorage.setItem("tema", "dark");
-        }
+    temaCurenta = localStorage.getItem("tema");
+    
+    if (temaCurenta) {
+      document.body.classList.add(temaCurenta);
+      document.getElementById("checkbox").checked = temaCurenta === "dark";
     }
-
-});
+    
+    document.getElementById("checkbox").addEventListener("change", function() {
+      if (this.checked) {
+        document.body.classList.add("dark");
+        localStorage.setItem("tema", "dark");
+      } else {
+        document.body.classList.remove("dark");
+        localStorage.removeItem("tema");
+      }
+    });
+  });
+  
